a6a7943fe2de1b73801879fe60cba2c6bd6c0b0c
IAMZAID22 <73696883+IAMZAID22@users.noreply.github.com>
2022-05-14 15:40:36 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,15 @@
+class Atm(object):
+    def __init__(self,atmcardno,pinno):
+        self.atmcardno = atmcardno
+        self.pinno = pinno
+
+    def start(self):
+        print("Started!")
+
+    def stop(self):
+        print("Stopped!") 
+
+card = int = input("Enter Card Number:")
+print(card.atmcardno)
+card = int = input("Enter Pin Number:")
+print(card.pinno)
